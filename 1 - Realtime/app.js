9143ded865b807354c30f371996c6281d7acf6c9
Christian Oliveira <christian.soliveira@outlook.com>
2020-05-14 20:19:26 -0300

    :rocket: Listen database, ordering and filtering data

--- 1 - Realtime/app.js
+++ 1 - Realtime/app.js
@@ -44,7 +44,7 @@ function criarCard() {
    * push(): cria um id unico e insere os dados dentro desse uid
    */
   refCard.push(card).then((snapshot) => {
-    adicionaCardATela(card, snapshot.key);
+    //adicionaCardATela(card, snapshot.key);
   });
 }
 
@@ -124,41 +124,83 @@ document.addEventListener("DOMContentLoaded", function () {
    * once(): retorna os dados lidos de uma url
    * snapshot: objeto retornado pela leitura
    */
+  // refCard.once("value").then((snapshot) => {
+  // Acessa um nó filho
+  // console.log("child", snapshot.child("-M7Ij5iPSG0Cj3oLkL60").val());
+  // // Checa se existe algo no snapshot
+  // console.log("exists()", snapshot.exists());
+  // // se existe o filho passado na url
+  // console.log(
+  //   "hasChild nome",
+  //   snapshot.hasChild("-M7Ij5iPSG0Cj3oLkL60/nome")
+  // );
+  // console.log(
+  //   "hasChild comentario",
+  //   snapshot.hasChild("-M7Ij5iPSG0Cj3oLkL60/comentario")
+  // );
+  // // se existe algum filho no nó
+  // console.log(
+  //   "hasChildren()",
+  //   snapshot.child("-M7Ij5iPSG0Cj3oLkL60").hasChildren()
+  // );
+  // // número de filhos no snapshot
+  // console.log("numChildren", snapshot.numChildren());
+  // // a chave desse snapshot/caminho
+  // console.log("keySnapshot", snapshot.key);
+  //   snapshot.forEach((data) => {
+  //     console.log("cardValue", data.val());
+  //     adicionaCardATela(data.val(), data.key);
+  //   });
+  // });
+  /**
+   * .ON
+   */
+  // refCard.on("value", (snapshot) => {
+  //   snapshot.forEach((data) => {
+  //     adicionaCardATela(data.val(), data.key);
+  //   });
+  // });
+  // refCard.on("child_added", (snapshot) => {
+  //   adicionaCardATela(snapshot.val(), snapshot.key);
+  // });
+  // refCard.on("child_changed", (snapshot, uid) => {
+  //   console.log(snapshot.key, uid);
+  // });
+  // refCard.on("child_removed", (snapshot) => {
+  //   console.log("child_removed", snapshot.key);
+  // });
 
-  refCard.once("value").then((snapshot) => {
-    // Acessa um nó filho
-    // console.log("child", snapshot.child("-M7Ij5iPSG0Cj3oLkL60").val());
+  /**
+   * Ordenação
+   *  E possivel utilizar apenas um metodo de ordenação por vêz
+   *
+   * .orderByChild('filho'): Ordenada pela propriedade filho passado como parametro
+   * .orderByKey(): ordenar pela chava dos nós
+   *
+   * .orderByValue(): ordena pelo valor de cada proprieda dentro do nó, não vale para nós pais
+   *
+   */
+  // refCard
+  //   .child("-M7KLs5qYRhzIMmFMQmb")
+  //   .orderByValue()
+  //   .on("child_added", (snapshot) => {
+  //     console.log(snapshot.key, snapshot.val());
+  //     // adicionaCardATela(snapshot.val(), snapshot.key);
+  //   });
 
-    // // Checa se existe algo no snapshot
-    // console.log("exists()", snapshot.exists());
+  /**
+   * WHERES / Filtros
+   *  .startAt() : Traz valores a partir do valor passado por parametro
+   *  .endAt(): Traz valores até o valor passado por parametro
+   *  .equalTo(): Traz valores correspondentes ao parametro
+   *  */
 
-    // // se existe o filho passado na url
-    // console.log(
-    //   "hasChild nome",
-    //   snapshot.hasChild("-M7Ij5iPSG0Cj3oLkL60/nome")
-    // );
-    // console.log(
-    //   "hasChild comentario",
-    //   snapshot.hasChild("-M7Ij5iPSG0Cj3oLkL60/comentario")
-    // );
-
-    // // se existe algum filho no nó
-    // console.log(
-    //   "hasChildren()",
-    //   snapshot.child("-M7Ij5iPSG0Cj3oLkL60").hasChildren()
-    // );
-
-    // // número de filhos no snapshot
-    // console.log("numChildren", snapshot.numChildren());
-
-    // // a chave desse snapshot/caminho
-    // console.log("keySnapshot", snapshot.key);
-
-    snapshot.forEach((data) => {
-      console.log("cardValue", data.val());
-      adicionaCardATela(data.val(), data.key);
+  refCard
+    .orderByChild("idade")
+    .equalTo(26)
+    .on("child_added", (snapshot) => {
+      adicionaCardATela(snapshot.val(), snapshot.key);
     });
-  });
 });
 
 /**
